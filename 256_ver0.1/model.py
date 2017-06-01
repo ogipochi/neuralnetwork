558c879847a7567a9c538a45bc9234ad55041aa1
Ogihara <pochi.0226.ogi@gmail.com>
2017-06-01 18:22:45 +0900

    Add files via upload

--- 256_ver0.1/model.py
+++ 256_ver0.1/model.py
@@ -3,44 +3,81 @@ from config import*
 #############################################################################################
 #モデルに使用する層を定義
 
-def conv_layer(bottom,name,num_filters,training_phase=True,kernel_size=[2,2]):
+#畳み込み層
+def conv_layer(
+    bottom,
+    name,
+    num_filters,
+    training_phase=True,
+    strides = (1,1),
+    dilation_rate=(1,1),
+    kernel_size=[2,2]):
+
+    #bottom:入力
+    #name:層のまとまりの名前
+    #num_filters:フイルターの名前
+    #training_phase:訓練か否か
+    #stride:畳み込み層のstride
+    #dilation_rate:畳み込みの値のとり方の隙間のとりぐあい
+    #               defalutの(1,1)は隙間なしを意味する
+    #kernel_size:カーネルサイズ
+
     with tf.variable_scope(name):
         first = tf.layers.conv2d(
             inputs=bottom,
             filters=num_filters,
             kernel_size=kernel_size,
+            dilation_rate=dilation_rate,
+            trainable=training_phase,
+            strides=strides,
             padding='SAME', name='conv1')
         second = tf.contrib.layers.batch_norm(inputs=first, center=True, scale=True, is_training=training_phase, scope='bn')
         relu = tf.nn.relu(second, 'relu')
         return relu
+#プーリング層
+def max_pool(
+    bottom,
+    name,
+    pool_size=[2,2],
+    strides=(1,1)):
+    #bottom:入力
+    #name:層のまとまりの名前
+    #pool_size:フィルタサイズ
+    #strides:stride
+    return tf.layers.max_pooling2d(inputs=bottom,pool_size=pool_size,strides=strides,padding='SAME',name=name)
 
-def max_pool(bottom,name,pool_size=[2,2]):
-    return tf.layers.max_pooling2d(inputs=bottom,pool_size=pool_size,strides=1,padding='SAME',name=name)
 
-
-def fc_layer(bottom,name,units,training_phase=True):
+def fc_layer(
+    bottom,
+    name,
+    units,
+    training_phase=True):
+    #bottom:入力
+    #name:層のまとまりの名前
+    #units:出力フィルタ数
+    #training_phase:値の訓練を行うか否か
     with tf.variable_scope(name):
-        first = tf.layers.dense(inputs=bottom,units=units,name='fc1')
+        first = tf.layers.dense(inputs=bottom,units=units,name='fc1',trainable=training_phase)
         second = tf.contrib.layers.batch_norm(inputs=first,center=True,scale=True,is_training=training_phase)
         relu = tf.nn.relu(second,'relu')
         return relu
 ################################################################################################
 #モデルの定義
 def vgg(image,training_phase):
-    conv1_1=conv_layer(bottom=image,name='conv1_1',num_filters=16,kernel_size=[2,2],training_phase=training_phase)
+    conv1_1=conv_layer(bottom=image,name='conv1_1',num_filters=32,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
     pool1 = max_pool(bottom=conv1_1,name='pool1',pool_size=[2,2])
-    conv2_1 = conv_layer(bottom=pool1,name='conv2_1',num_filters=24,kernel_size=[2,2],training_phase=training_phase)
-    conv2_2 = conv_layer(bottom=conv2_1,name='conv2_2',num_filters=28,kernel_size=[2,2],training_phase=training_phase)
+    conv2_1 = conv_layer(bottom=pool1,name='conv2_1',num_filters=64,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
+    conv2_2 = conv_layer(bottom=conv2_1,name='conv2_2',num_filters=64,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
     pool2 = max_pool(bottom=conv2_2,name='pool2')
-    conv3_1 = conv_layer(bottom=pool2,name='conv3_1',num_filters=32,kernel_size=[2,2],training_phase=training_phase)
-    conv3_2=conv_layer(bottom=conv3_1,name='conv3_2',num_filters=36,kernel_size=[2,2],training_phase=training_phase)
+    conv3_1 = conv_layer(bottom=pool2,name='conv3_1',num_filters=128,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
+    conv3_2=conv_layer(bottom=conv3_1,name='conv3_2',num_filters=128,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
     pool3 = max_pool(bottom=conv3_2,name='pool3')
-    conv4_1 = conv_layer(bottom=pool3,name='conv4_1',num_filters=48,kernel_size=[2,2],training_phase=training_phase)
-    conv4_2 = conv_layer(bottom=conv4_1,name='conv4_2',num_filters=56,kernel_size=[2,2],training_phase=training_phase)
+    conv4_1 = conv_layer(bottom=pool3,name='conv4_1',num_filters=256,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
+    conv4_2 = conv_layer(bottom=conv4_1,name='conv4_2',num_filters=256,kernel_size=[3,3],strides = (2,2),training_phase=training_phase)
     pool4 = max_pool(bottom=conv4_2,name='pool4')
     flatten = slim.layers.flatten(inputs=pool4,scope='flatten')
-    fc1=fc_layer(bottom=flatten,name='fc1',units=64,training_phase=training_phase)
-    fc2 = fc_layer(bottom=fc1, name='fc2', units=64,training_phase=training_phase)
+    fc1=fc_layer(bottom=flatten,name='fc1',units=256,training_phase=training_phase)
+    fc2 = fc_layer(bottom=fc1, name='fc2', units=256,training_phase=training_phase)
     fc3 = fc_layer(bottom=fc2,name='fc3',units=NUM_CLASSES,training_phase=training_phase)
     return  fc3
 ##################################################################################################
